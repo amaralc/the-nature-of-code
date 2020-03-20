e2d221bde1aa8f8b47dbaae031627af05e011ce8
amaralc <amaral.calil@gmail.com>
2020-03-20 16:13:55 -0300

    CONFIG / Configure initial files

---
+++ public/sketch.js
@@ -0,0 +1,25 @@
+var xoff = 0;
+var yoff = 0;
+var doff = 0;
+
+function setup() {
+
+  createCanvas(innerWidth,innerHeight);
+  noiseSeed(100);
+}
+
+function draw() {
+
+
+  ellipse(
+    map(noise(xoff), 0, 1, 0, width),
+    map(noise(yoff),0,1,0,height),
+    map(noise(doff),0,1,20,50),
+    map(noise(doff),0,1,20,50)
+  );
+  xoff += 0.007;
+  yoff += 0.009;
+  doff += 0.011;
+
+}
+
